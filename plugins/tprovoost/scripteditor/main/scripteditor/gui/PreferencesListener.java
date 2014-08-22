2955e72207e78fe8a5442b81d9c8d786123f20fa
Wei Ouyang <oeway007@gmail.com>
2014-08-22 09:38:00 +0800

    remove redundent files

--- plugins/tprovoost/scripteditor/main/scripteditor/gui/PreferencesListener.java
+++
@@ -1,7 +0,0 @@
-package plugins.tprovoost.scripteditor.gui;
-
-import java.util.EventListener;
-
-interface PreferencesListener extends EventListener {
-	void preferencesChanged();
-}
--- plugins/tprovoost/scripteditor/main/scripteditor/main/ScriptListener.java
+++
@@ -1,8 +0,0 @@
-package plugins.tprovoost.scripteditor.main;
-
-public interface ScriptListener
-{
-    public void evaluationStarted();
-    
-    public void evaluationOver();
-}
